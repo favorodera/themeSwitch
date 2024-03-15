f40a0c16d6a7908c7af0828002ecae3bcb21024d
Favour Chidera Emeka <favorodera@gmail.com>
2024-03-15 21:30:05 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,37 @@
 `strict`
 
-document.querySelector(`.lightMode`).style.cursor = `pointer`
+// TOGGLES DARK MODE
+function toggleDarkMode() {
 
-function darkMode() {
+    // CHANGES THE LIGHT MODE BUTTON
     document.querySelector(`.lightMode`);
     let lightModeCss = {
         backgroundColor: `rgba(255, 255, 255, 1)`,
         cursor: `initial`,
-        display:`contents`,
-        marginLeft: `21px`,
-        marginRight: `33px`,
+        display: `contents`,
     };
-    Object.assign(document.querySelector(`.lightMode`).style, lightModeCss )
-document.querySelector(`.darkMode`).style.backgroundColor = `rgba(0, 41, 255, 1)`;
-document.querySelector(`.themeChangerButtonsContainer`).style.backgroundColor = `rgba(44, 44, 44, 1)`;
+    Object.assign(document.querySelector(`.lightMode`).style, lightModeCss);
+
+    // CHANGES THE LIGHTMODE AND DARKMODE BUTTONS CONTAINER
+    document.querySelector(`.themeChangerButtonsContainer`)
+    let themeChangerButtonsContainerCss = {
+        backgroundColor: `black`,
+        justifyContent: `space-evenly`,
+        backgroundColor: `rgba(44, 44, 44, 1)`,
+        paddingLeft: `21px`
+    };
+    Object.assign(document.querySelector(`.themeChangerButtonsContainer`).style, themeChangerButtonsContainerCss);
+
+    // CHANGES THE DARKMODE BUTTON
+    document.querySelector(`.darkMode`).style
+    let darkModeCss = {
+        backgroundColor: `rgba(0, 41, 255, 1)`,
+        display: `block`,
+        margin: `6px 6px 6px 33px`,
+        cursor: `pointer`
+    };
+    Object.assign(document.querySelector(`.darkMode`).style, darkModeCss);
+
+    // CHANGES THE BACKGROUND COLOR OF THE PARENT CONTAINER WHICH CAN ALSO SUBSTITUTED WITH THE BODY WHEN USING IN REAL PROJECTS.
+    document.querySelector(`.themeChangerContainer`).style.backgroundColor = `rgba(32, 32, 32, 1)`;
 }
