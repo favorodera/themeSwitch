c4745277417fdb5114f6ce2aa70a96b370a3a635
Favour Chidera Emeka <favorodera@gmail.com>
2024-03-16 17:31:30 +0100

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,64 @@
 `strict`
 
-// TOGGLES DARK MODE
 function toggleDarkMode() {
 
-    // CHANGES THE LIGHT MODE BUTTON
-    document.querySelector(`.lightMode`);
     let lightModeCss = {
-        backgroundColor: `rgba(255, 255, 255, 1)`,
         cursor: `initial`,
         display: `contents`,
     };
     Object.assign(document.querySelector(`.lightMode`).style, lightModeCss);
 
-    document.querySelector(`.lightMode > svg`).style.marginLeft = `19px`;
-
-    // CHANGES THE LIGHTMODE AND DARKMODE BUTTONS CONTAINER
-    document.querySelector(`.themeChangerButtonsContainer`)
-    let themeChangerButtonsContainerCss = {
-        backgroundColor: `black`,
-        justifyContent: `space-evenly`,
-        backgroundColor: `rgba(44, 44, 44, 1)`,
-    };
-    Object.assign(document.querySelector(`.themeChangerButtonsContainer`).style, themeChangerButtonsContainerCss);
-
-    // CHANGES THE DARKMODE BUTTON
-    document.querySelector(`.darkMode`).style
     let darkModeCss = {
         backgroundColor: `rgba(0, 41, 255, 1)`,
-        display: `flex`,
-        justifyContent: `center`,
-        alignItems: `center`,
-        margin: `6px 6px 6px 33px`,
-        cursor: `pointer`
+        display: `block`,
+        width: `50px`,
+        height: `50px`,
+        borderRadius: `100px`,
+        margin: `6px 6px 6px 0px`,
+        cursor: `pointer`,
+        border: `none`,
     };
     Object.assign(document.querySelector(`.darkMode`).style, darkModeCss);
 
-    document.querySelector(`.darkMode > svg`).style.marginRight = ``;
+    document.querySelector(`.lightModeSvg`).style.margin = `19px 33px 19px 21px`;
 
-    // CHANGES THE COLOR OF THE MOON SVG
+    document.querySelector(`.themeChangerButtonsContainer`).style.backgroundColor = `rgba(44, 44, 44, 1)`;
+
+    document.querySelector(`.darkModeSvg`).style.margin = `13px`;
+
     document.querySelector(`.darkModeSvgPath`).style.fill = `rgba(255, 255, 255, 1)`;
 
-    // CHANGES THE BACKGROUND COLOR OF THE PARENT CONTAINER WHICH CAN ALSO SUBSTITUTED WITH THE BODY WHEN USING IN REAL PROJECTS.
     document.querySelector(`.themeChangerContainer`).style.backgroundColor = `rgba(32, 32, 32, 1)`;
 
-    // CHANGES THE BACKGROUND COLOR OF THE BODY
     document.body.style.backgroundColor = `rgba(32, 32, 32, 1)`;
-
 };
 
 
-// TOGGLES LIGHTMODE
 function toggleLightMode() {
 
-    // CHANGES THE DARKMODE BUTTON
-    document.querySelector(`.darkMode`).style
-    let darkModeCss = {
-        backgroundColor: `rgba(0, 41, 255, 1)`,
-        display: `contents`,
-        cursor: `initial`,
-    };
-    Object.assign(document.querySelector(`.darkMode`).style, darkModeCss);
-
-    document.querySelector(`.darkMode > svg`).style.marginRight = `21px`;
-
-    // CHANGES THE LIGHT MODE BUTTON
-    document.querySelector(`.lightMode`);
     let lightModeCss = {
-        backgroundColor: `rgba(0, 41, 255, 1)`,
         cursor: `pointer`,
-        display: `flex`,
-        justifyContent: `center`,
-        alignItems: `center`,
-        margin: `6px 32px 6px 6px`,
+        display: `block`,
+        width: `50px`,
+        height: `50px`,
+        borderRadius: `100px`,
+        backgroundColor: `rgba(0, 41, 255, 1)`,
+        border: `none`,
+        margin: `6px 32px 6px 8px`
     };
     Object.assign(document.querySelector(`.lightMode`).style, lightModeCss);
 
-    document.querySelector(`.lightMode > svg`).style.marginLeft = ``;
+    document.querySelector(`.lightModeSvg`).style.margin = `13px`
 
-    // CHANGES THE LIGHTMODE AND DARKMODE BUTTONS CONTAINER
-    document.querySelector(`.themeChangerButtonsContainer`)
-    let themeChangerButtonsContainerCss = {
-        backgroundColor: `rgba(255, 255, 255, 1)`,
-        padding: ``,
-        justifyContent: ``
-    };
-    Object.assign(document.querySelector(`.themeChangerButtonsContainer`).style, 
-    themeChangerButtonsContainerCss);
+    document.querySelector(`.darkMode`).style.display = `contents`;
 
-    // REVERTS THE COLOR OF THE MOON SVG
-    document.querySelector(`.darkModeSvgPath`).style.fill = ``;
+    document.querySelector(`.darkModeSvg`).style.margin = `19px 20px 19px 0px`;
 
-    // CHANGES THE BACKGROUND COLOR OF THE PARENT CONTAINER WHICH CAN ALSO SUBSTITUTED WITH THE BODY WHEN USING IN REAL PROJECTS.
+    document.querySelector(`.themeChangerButtonsContainer`).style.backgroundColor = `rgba(255, 255, 255, 1)`;
+
+    document.querySelector(`.darkModeSvgPath`).style.fill = `#bababa`;
+
     document.querySelector(`.themeChangerContainer`).style.backgroundColor = `rgba(226, 224, 224, 1)`;
 
-    // CHANGES THE BACKGROUND COLOR OF THE BODY
     document.body.style.backgroundColor = `rgba(226, 224, 224, 1)`;
-
 }
